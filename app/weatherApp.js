f452e04a7dfdab4adca9835ee0e6b334e32328f7
Deborah Oni <deborahoooni@gmail.com>
2017-03-14 00:11:09 +0100

    write information of the app

--- app/weatherApp.js
+++ app/weatherApp.js
@@ -1,3 +1,8 @@
+// 
+// A simple command line application that consumes a Public API using a HTTP client library.
+//A weather forecast application that calls api details from https://darksky.net
+
+
 var forecast = require('./forecastApi.js');
 
 forecast.forecastApi(6.5244,3.3792);
